063f96dd8f743c963a8530c2c32966923551abfa
JamesKm08 <66277467+JamesKm08@users.noreply.github.com>
2023-09-21 13:46:16 +0300

    feat: create the nft contract

---
+++ contracts/DevNFT.sol
@@ -0,0 +1,17 @@
+ // SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+
+contract DeveNFT is ERC721Enumerable {
+    // Initialize the ERC721 Contract
+    constructor() ERC721(
+        "Devs","D"
+    ) {}
+
+    // Add a public mint function to call the NFT
+    function mint() public {
+        _safeMint(msg.sender, totalSupply());
+        }
+}
+
