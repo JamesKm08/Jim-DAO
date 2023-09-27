d1753ebff4299e8aa5c6f954060a51a7c45db064
JamesKm08 <66277467+JamesKm08@users.noreply.github.com>
2023-09-27 11:03:29 +0300

    build: create the deployment script

---
+++ scripts/Deploy.ts
@@ -0,0 +1,56 @@
+import hre from "hardhat"
+
+async function sleep(ms: number) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  }
+
+async function main() {
+    // Deploy the NFT Contract
+    const nftContract = await hre.ethers.deployContract("DevNFT");
+    await nftContract.waitForDeployment();
+    console.log("DevNFT deployed to : ", nftContract.target);
+
+    // Deploy the fake NFT Market
+    const myNFTMkt = await hre.ethers.deployContract("myNFTMkt");
+    await myNFTMkt.waitForDeployment();
+    console.log("myNFTMkt deployed to : ", myNFTMkt.target);
+
+    // Deploy the DAO Contract
+    const amount = hre.ethers.parseEther("2");
+    const daoContract = await hre.ethers.deployContract("DevDAO", [
+        myNFTMkt.target,
+        nftContract.target,
+        ], {value: amount,});
+    await daoContract.waitForDeployment();
+    console.log("CryptoDevsDAO deployed to:", daoContract.target);
+
+    // Sleep for 30 seconds to let Etherscan catch up with the deployments
+    await sleep(30 * 1000);
+
+    // Verify the NFT Contract
+    await hre.run("verify:verify", {
+        address: nftContract.target,
+        constructorArguments: [],
+    });
+
+    // Verify the Fake Marketplace Contract
+    await hre.run("verify:verify", {
+        address: myNFTMkt.target,
+        constructorArguments: [],
+    });
+
+    // Verify the DAO Contract
+    await hre.run("verify:verify", {
+        address: daoContract.target,
+        constructorArguments: [
+        myNFTMkt.target,
+        nftContract.target,
+        ],
+    });
+
+    // Check and handle errors
+    main().catch((error)=>{
+        console.log(error);
+        process.exitCode = 1;
+    });
+}
