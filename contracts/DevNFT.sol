fedc9c18322219e0f98c626e2986e312f186e2e3
JamesKm08 <66277467+JamesKm08@users.noreply.github.com>
2023-09-27 11:11:49 +0300

    fix: Word correction from DeveNFT to DevNFT

--- contracts/DevNFT.sol
+++ contracts/DevNFT.sol
@@ -3,7 +3,7 @@ pragma solidity 0.8.19;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 
-contract DeveNFT is ERC721Enumerable {
+contract DevNFT is ERC721Enumerable {
     // Initialize the ERC721 Contract
     constructor() ERC721(
         "Devs","D"
